2fec9f879ca760906e64450eae79e52673adb3b9
Saquib Anjum <134352662+Saquib-Anjum@users.noreply.github.com>
2026-08-15 11:59:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const line = document.getElementById("line");
+
+let angle = 0;
+setInterval(() => {
+    angle += 2; // Increase angle by 2 degrees (or adjust speed)
+    line.style.transform = `rotate(${angle}deg)`;
+}, 20); // updates every 20ms
+
